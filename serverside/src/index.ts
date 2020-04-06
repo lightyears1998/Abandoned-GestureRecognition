e8e92e03663fe4f315e0a9d0639d778d25e019aa
lightyears1998 <lightyears1998@hotmail.com>
2020-04-06 15:37:42 +0800

    Make bodyParser work

--- serverside/src/index.ts
+++ serverside/src/index.ts
@@ -1,3 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.all("/", (req, res) => {
+  console.log(req.body);
+  res.send("goodbye.");
+});
+
+app.listen(6040);
